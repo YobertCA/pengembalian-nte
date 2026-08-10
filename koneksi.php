20e083d40bd306dc01aea134e930a0766212b9b1
YobertCA <148619597+YobertCA@users.noreply.github.com>
2026-08-10 13:06:11 +0700

    Change database connection settings
    Updated database connection details for InfinityFree.

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,16 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "db_pengembalian";
+$host = "sql307.infinityfree.com";
+$user = "if0_42613645";
+$password = "PASSWORD MYSQL INFINITYFREE";
+$database = "if0_42613645_ba_nte";
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
+
+mysqli_set_charset($conn, "utf8mb4");
 
 ?>
